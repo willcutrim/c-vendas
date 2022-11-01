d2efbc6582625a9b440e8f6384a4d57e71166004
willcutrim <willyam894@gmail.com>
2022-10-31 23:02:47 -0300

    Update: adicionado paginação na aba de historico de vendas

--- vendas_app/views.py
+++ vendas_app/views.py
@@ -8,6 +8,7 @@ from .forms import FormProduto, FormCategoria
 from .serializers import CarrinhoSerializer
 from django.contrib import messages
 import requests
+from django.core.paginator import Paginator
 
 class CarrinhoController(APIView):
 
@@ -57,14 +58,18 @@ def home(request):
 
 def historico_de_vendas(request):
     
-    vendas_q = Carrinho.objects.filter().order_by('-data_compra')[:10]
+    vendas_q = Carrinho.objects.filter().order_by('-data_compra')
+    p = Paginator(vendas_q, 10)
+    page = request.GET.get('page')
+    vendas = p.get_page(page)
+
     
     start_date = request.GET.get('data_inicial')
     end_date = request.GET.get('data_final')
     if start_date and end_date:
-        vendas_q = Carrinho.objects.filter(data_compra__range=[start_date, end_date])
+        vendas = Carrinho.objects.filter(data_compra__range=[start_date, end_date])
     
-    return render(request, 'html/vendas.html', {'vendas_q': vendas_q})
+    return render(request, 'html/vendas.html', {'vendas_q': vendas_q, 'vendas': vendas, 'p': p})
 
 
 def vendas_detalhes(request, pk):
